2589f1c5239abb501a884d40b579b8093bf628b5
omlan99 <omlan10616@gmail.com>
2024-12-01 13:53:40 +0600

    added latestNews section with marqueee

--- src/Component/LatestNews.jsx
+++ src/Component/LatestNews.jsx
@@ -1,7 +1,16 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const LatestNews = () => {
     return (
-        <div>
-            This
+        <div className="p-4 flex bg-base-200 ">
+            <p className=" text-white "><span className="py-2 px-6 bg-[#D72050]">Latest</span></p>
+            <Marquee pauseOnHover={true} className="space-x-10">
+                <Link to='/news'>Match Highlights: Germany vs Spain — as it happened   1! </Link>
+                <Link to='/news'>Match Highlights: Germany vs Spain — as it happened   2! </Link>
+                <Link to='/news'>Match Highlights: Germany vs Spain — as it happened   3! </Link>
+                <Link to='/news'>Match Highlights: Germany vs Spain — as it happened   4! </Link>
+            </Marquee>
         </div>
     );
 };
